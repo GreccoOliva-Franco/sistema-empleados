d54a0f3dd7f91e27ab5f465f3843c6801ad6b797
Franco Grecco <srgrecco93@gmail.com>
2021-07-07 20:03:47 -0300

    Creo instancia Flask

--- src/app.py
+++ src/app.py
@@ -1,2 +1,10 @@
+from os import name
 from flask import Flask
-print("Hello")
+from flask import render_template
+from flaskext.mysql import MySQL
+
+app = Flask(__name__)
+mysql = MySQL()
+
+if __name__ == '__main__':
+  app.run(debug=True)
