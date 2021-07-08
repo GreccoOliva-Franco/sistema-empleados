c3f5d29bf91484ed5ceb8a981a6b521f33094b35
Franco Grecco <srgrecco93@gmail.com>
2021-07-07 21:12:51 -0300

    Test conexion BD y agregado de datos

--- src/app.py
+++ src/app.py
@@ -6,5 +6,27 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 mysql = MySQL()
 
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_PORT'] = 3337
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'empleados'
+
+mysql.init_app(app)
+
+@app.route('/')
+def index():
+  sql = "insert into empleados (nombre, correo, foto) values ('Franco', 'mail@mail.com', 'fotoDeFranco.jpg');"
+
+  con = mysql.connect()
+  cur = con.cursor()
+  cur.execute(sql)
+
+  con.commit()
+
+  return render_template('empleados/index.html')
+
+
 if __name__ == '__main__':
   app.run(debug=True)
+
